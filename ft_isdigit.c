a482d2de4018966a1e88dca760e405668e6bb5b1
Sergey Kuznetsov <skuznets@c2r14s2.42yerevan.am>
2024-01-15 20:05:12 +0400

    kkk

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skuznets <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 19:54:11 by skuznets          #+#    #+#             */
-/*   Updated: 2024/01/15 20:02:29 by skuznets         ###   ########.fr       */
+/*   Created: 2024/01/15 19:58:12 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/15 20:01:12 by skuznets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <libft.h>
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
