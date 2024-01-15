85c62779e95a681938e68195afd908dd6e7eb4c3
Sergey Kuznetsov <kuuuzya@ya.ru>
2024-01-15 23:46:49 +0400

    Added ft_isprint.c and ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,18 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skuznets <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 23:39:27 by skuznets          #+#    #+#             */
-/*   Updated: 2024/01/15 23:44:29 by skuznets         ###   ########.fr       */
+/*   Created: 2024/01/15 23:45:27 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/15 23:46:43 by skuznets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h> 
+#include <libft.h>
 
-int	ft_isascii(int c)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	return (c >= 0 && c <= 127);
+	char	*ptr;
+
+	ptr = b;
+	while (len--)
+		*ptr++ = (unsigned char)c;
+	return (b);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: skuznets <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 19:54:11 by skuznets          #+#    #+#             */
-/*   Updated: 2024/01/15 23:41:10 by skuznets         ###   ########.fr       */
+/*   Updated: 2024/01/15 23:44:43 by skuznets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 size_t	ft_strlen(const char *s);
 
