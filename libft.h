d899ae0ed87c092bb0c62db3e93545b20055a558
Sergey Kuznetsov <skuznets@c2r8s6.42yerevan.am>
2024-01-22 17:41:25 +0400

    d

--- libft.h
+++ libft.h
@@ -5,16 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: skuznets <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 19:54:11 by skuznets          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/01/17 17:28:38 by skuznets         ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2024/01/16 15:41:20 by skuznets         ###   ########.fr       */
-=======
-/*   Updated: 2024/01/17 17:16:46 by skuznets         ###   ########.fr       */
->>>>>>> 9b879e27fa56ddf045cd3c37a5a5a698733b94fb
->>>>>>> 25512b9b5ddb42f07d62ab5b0aa219ecd7325172
+/*   Created: 2024/01/22 17:36:28 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/22 17:36:57 by skuznets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +21,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int 	ft_toupper(int c);
+int		ft_toupper(int c);
 int		ft_tolower(int c);
 
 size_t	ft_strlen(const char *s);
